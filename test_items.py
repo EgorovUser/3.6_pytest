4820317783b97b487e208692074dcd529f9102bb
Artem Egorov <sparta7499@gmail.com>
2020-10-11 12:50:26 +0300

    Добавил ассерт т.к. не заметил сначала))

--- test_items.py
+++ test_items.py
@@ -6,4 +6,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/';
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.implicitly_wait(5)
-    browser.find_element_by_css_selector('.btn-add-to-basket')
+    button = browser.find_elements_by_css_selector('.btn-add-to-basket')
+    assert len(button) > 0 ,"Элемент не найден"
