91cbf9f18ac146fda31e8781292a1c3ab25466d6
Artem Egorov <sparta7499@gmail.com>
2020-10-11 12:52:09 +0300

    Изменил название метода

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/';
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket_exist(browser):
     browser.get(link)
     browser.implicitly_wait(5)
     button = browser.find_elements_by_css_selector('.btn-add-to-basket')
